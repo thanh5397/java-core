5d4efbe811714c6d55946a0d5f5980052a2e326c
phamvanthanh <phamvanthanh@luvina.net>
2022-03-29 17:29:45 +0700

    29032022

--- news/src/main/java/com/laptrinhjavaweb/api/NewAPI.java
+++ news/src/main/java/com/laptrinhjavaweb/api/NewAPI.java
@@ -1,22 +1,33 @@
 package com.laptrinhjavaweb.api;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.laptrinhjavaweb.dto.NewDTO;
 
  
-@Controller
-
+@CrossOrigin
+@RestController
 public class NewAPI {
 
-        @RequestMapping(value = "/new", method = RequestMethod.POST)
-        @ResponseBody
-        public NewDTO createNew(@RequestBody NewDTO model) {
-                return model;
-        }
+
+	@PostMapping(value = "/new")
+	public NewDTO createNew(@RequestBody NewDTO model) {
+		return model;
+	}
+	
+	@PutMapping(value = "/new")
+	public NewDTO updateNew(@RequestBody NewDTO model) {
+		return model;
+	}
+	
+	@DeleteMapping(value = "/new")
+	public void deleteNew(@RequestBody long[] ids) {
+		
+	}
 
 }
--- news/src/main/java/com/laptrinhjavaweb/dto/NewDTO.java
+++ news/src/main/java/com/laptrinhjavaweb/dto/NewDTO.java
@@ -2,9 +2,11 @@ package com.laptrinhjavaweb.dto;
 
 public class NewDTO {
 
-    private String title;
-
-    private String content;  
+	private String title;
+	private String content;
+	private String shortDescription;
+	private String categoryCode;
+	private String thumbnail;
 
     public String getTitle() {
 
@@ -30,4 +32,27 @@ public class NewDTO {
 
     }
 
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getCategoryCode() {
+		return categoryCode;
+	}
+
+	public void setCategoryCode(String categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
 }
