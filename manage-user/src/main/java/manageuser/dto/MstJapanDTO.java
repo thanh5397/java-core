3808a15a6be7b05035a428d2312ec7bcd819e4b2
ThanhPV <thanhpv997@gmail.com>
2022-06-14 07:08:40 +0700

    14062022

--- manage-user/src/main/java/manageuser/dto/MstJapanDTO.java
+++ manage-user/src/main/java/manageuser/dto/MstJapanDTO.java
@@ -10,9 +10,6 @@ import manageuser.entities.TblUserEntity;
 public class MstJapanDTO {
 	private String codeLevel;
 	private String nameLevel;
-	private Date startDate;
-	private Date endDate;
-	private int total;
 	private List<TblDetailUserJapanEntity> tblDetailUserJapanEntities = new ArrayList<TblDetailUserJapanEntity>();
 	
 	public String getCodeLevel() {
@@ -26,24 +23,6 @@ public class MstJapanDTO {
 	}
 	public void setNameLevel(String nameLevel) {
 		this.nameLevel = nameLevel;
-	}
-	public Date getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-	public Date getEndDate() {
-		return endDate;
-	}
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	public int getTotal() {
-		return total;
-	}
-	public void setTotal(int total) {
-		this.total = total;
 	}
 	public List<TblDetailUserJapanEntity> getTblDetailUserJapanEntities() {
 		return tblDetailUserJapanEntities;
--- manage-user/src/main/java/manageuser/dto/TblDetailUserJapanDTO.java
+++ manage-user/src/main/java/manageuser/dto/TblDetailUserJapanDTO.java
@@ -2,7 +2,14 @@ package manageuser.dto;
 
 import java.util.Date;
 
+import manageuser.entities.MstJapanEntity;
+import manageuser.entities.TblDetailUserJapanId;
+import manageuser.entities.TblUserEntity;
+
 public class TblDetailUserJapanDTO {
+	private TblDetailUserJapanId tblDetailUserJapanId;
+	private MstJapanEntity mstJapanEntity;
+	private TblUserEntity tblUserEntity;
 	private Date startDate;
 	private Date endDate;
 	private int total;
@@ -24,5 +31,23 @@ public class TblDetailUserJapanDTO {
 	}
 	public void setTotal(int total) {
 		this.total = total;
+	}
+	public TblDetailUserJapanId getTblDetailUserJapanId() {
+		return tblDetailUserJapanId;
+	}
+	public void setTblDetailUserJapanId(TblDetailUserJapanId tblDetailUserJapanId) {
+		this.tblDetailUserJapanId = tblDetailUserJapanId;
+	}
+	public MstJapanEntity getMstJapanEntity() {
+		return mstJapanEntity;
+	}
+	public void setMstJapanEntity(MstJapanEntity mstJapanEntity) {
+		this.mstJapanEntity = mstJapanEntity;
+	}
+	public TblUserEntity getTblUserEntity() {
+		return tblUserEntity;
+	}
+	public void setTblUserEntity(TblUserEntity tblUserEntity) {
+		this.tblUserEntity = tblUserEntity;
 	}
 }
